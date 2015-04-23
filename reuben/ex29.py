4f866b6f9710cfd343032d1db6cfa778832ed74d
Reubenscube <RKarchem@gmail.com>
2015-04-22 20:43:05 -0400

    Test and Logic

---
+++ reuben/ex29.py
@@ -0,0 +1,42 @@
+people = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+
+if people > cats:
+	print "not many cats! The world is saved!"	
+
+if people < dogs:
+	print "The world is drooled on!"
+
+if people > dogs:
+	print "The world is dry!"	
+
+
+dogs += 5
+#dogs = dogs + 5
+
+if people >= dogs:
+	print "People are greater than or equal to dogs."
+
+if people <= dogs:
+	print "People are less than or equal to dogs."
+
+
+if people == dogs:
+	print "People are dogs."
+
+#Study Drills
+#1 What do you think the if does to the code under it?
+#'If' prints the code below it if the statement is true
+#2 WHy does the code under the if need to be indented 4 spaces?
+#The indent ensures that the print is subject to the if statement.
+#3 What happens if it isn't indented?
+#The if statement will print T/F, and the statement won't print.
+
+
+
+
